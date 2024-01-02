9e6e5227c1dfaef94e11cd54d67b5f6c96d63be4
Dherkyan R. Silva <dherkyan@gmail.com>
2024-01-02 05:53:20 -0300

    [Aula-30] Tratando exceções lançadas por no processo de validação de campos

---
+++ src/main/java/com/softwarefoundation/parkingapi/web/exception/ApiExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.softwarefoundation.parkingapi.web.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException exception, HttpServletRequest request, BindingResult result) {
+        log.error("methodArgumentNotValidException: ", exception);
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Campo(s) inválidos", result));
+
+    }
+
+}
