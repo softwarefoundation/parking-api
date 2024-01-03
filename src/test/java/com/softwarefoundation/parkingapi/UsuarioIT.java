f505486a0a79bb4a5c158cb5dbc9c9daae6e6e9b
Dherkyan R. Silva <dherkyan@gmail.com>
2024-01-03 02:53:42 -0300

    [Aula-41] Teste ponta a ponta para criar um novo usuário

--- src/test/java/com/softwarefoundation/parkingapi/UsuarioIT.java
+++ src/test/java/com/softwarefoundation/parkingapi/UsuarioIT.java
@@ -1,7 +1,12 @@
 package com.softwarefoundation.parkingapi;
 
+import com.softwarefoundation.parkingapi.entity.dto.UsuarioCreateDto;
+import com.softwarefoundation.parkingapi.entity.dto.UsuarioResponseDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -12,5 +17,22 @@ public class UsuarioIT {
 
     @Autowired
     private WebTestClient testClient;
+
+    @Test
+    public void createUsuarioComUsernamePasswordValidosRetornarUsuarioCriadoComStatus201() {
+        UsuarioResponseDto responseBody = this.testClient
+                .post()
+                .uri("/api/v1/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDto("tod@gmail.com", "123456"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(UsuarioResponseDto.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getId()).isNotNull();
+        Assertions.assertThat(responseBody.getUsername()).isEqualTo("tod@gmail.com");
+    }
 
 }
