fbd48edb96965b3273a149053520dc7f97cbca7e
Dherkyan R. Silva <dherkyan@gmail.com>
2024-01-03 19:15:50 -0300

    [Aula-49] Criando o objeto de resposta JwtToken

---
+++ src/main/java/com/softwarefoundation/parkingapi/jwt/JwtToken.java
@@ -0,0 +1,16 @@
+package com.softwarefoundation.parkingapi.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtToken {
+
+    private String token;
+
+}
