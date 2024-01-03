ccc41eba5871efdfc169379854538dca94ac986c
Dherkyan R. Silva <dherkyan@gmail.com>
2024-01-03 02:35:28 -0300

    [Aula-40] Configurando o ambiente de testes

---
+++ src/main/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+insert into TB01_USUARIO(username, password, role) values('ana@gmail.com','123456','ROLE_ADMIN');
+insert into TB01_USUARIO(username, password, role) values('bob@gmail.com','123456','ROLE_CLIENTE');
+insert into TB01_USUARIO(username, password, role) values('tom@gmail.com','123456','ROLE_CLIENTE');
