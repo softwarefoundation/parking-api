2c4b733d5b98b6140770a9a2e6124d9c1b1cc358
Dherkyan R. Silva <dherkyan@gmail.com>
2024-01-03 03:03:54 -0300

    [Aula-42] Teste para verificar exceções de validações (422)

--- src/test/java/com/softwarefoundation/parkingapi/UsuarioIT.java
+++ src/test/java/com/softwarefoundation/parkingapi/UsuarioIT.java
@@ -2,10 +2,12 @@ package com.softwarefoundation.parkingapi;
 
 import com.softwarefoundation.parkingapi.entity.dto.UsuarioCreateDto;
 import com.softwarefoundation.parkingapi.entity.dto.UsuarioResponseDto;
+import com.softwarefoundation.parkingapi.web.exception.ErrorMessage;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -33,6 +35,64 @@ public class UsuarioIT {
         Assertions.assertThat(responseBody).isNotNull();
         Assertions.assertThat(responseBody.getId()).isNotNull();
         Assertions.assertThat(responseBody.getUsername()).isEqualTo("tod@gmail.com");
+    }
+
+    @Test
+    public void createUsuarioComUsernameInvalidosRetornarErrorMessageComStatus422() {
+        ErrorMessage responseBody = this.testClient
+                .post()
+                .uri("/api/v1/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDto("", "123456"))
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value())
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value());
+
+        responseBody = this.testClient
+                .post()
+                .uri("/api/v1/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDto("tod", "123456"))
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value())
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value());
+    }
+
+    @Test
+    public void createUsuarioComPasswordInvalidosRetornarErrorMessageComStatus422() {
+        ErrorMessage responseBody = this.testClient
+                .post()
+                .uri("/api/v1/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDto("alice@gmail.com", "123"))
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value())
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value());
+
+        responseBody = this.testClient
+                .post()
+                .uri("/api/v1/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDto("alice@gmail.com", "12345678"))
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value())
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value());
     }
 
 }
