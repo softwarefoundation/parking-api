e4b86953150aa605fbba003007e5c5fe0a4e28c0
Dherkyan R. Silva <dherkyan@gmail.com>
2024-01-04 03:41:13 -0300

    [Aula-53] O controller de Autenticação

---
+++ src/main/java/com/softwarefoundation/parkingapi/entity/dto/UsuarioLoginDto.java
@@ -0,0 +1,26 @@
+package com.softwarefoundation.parkingapi.entity.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioLoginDto {
+
+    @NotBlank(message = "Informe o e-mail")
+    @Email(message = "Formato do e-mail inválido", regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
+    private String username;
+
+    @NotBlank(message = "Informe a senha")
+    @Size(min = 6, max = 6)
+    private String password;
+
+}
