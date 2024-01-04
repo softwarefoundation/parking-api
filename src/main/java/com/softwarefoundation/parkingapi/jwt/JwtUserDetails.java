2199acf7dfc3d3b8d77e09b36e7a1bd95422e5f9
Dherkyan R. Silva <dherkyan@gmail.com>
2024-01-04 03:13:28 -0300

    [Aula-51] A classe JwtUserDetails

---
+++ src/main/java/com/softwarefoundation/parkingapi/jwt/JwtUserDetails.java
@@ -0,0 +1,24 @@
+package com.softwarefoundation.parkingapi.jwt;
+
+import com.softwarefoundation.parkingapi.entity.Usuario;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+public class JwtUserDetails extends User {
+
+    private Usuario usuario;
+
+    public JwtUserDetails(Usuario usuario) {
+        super(usuario.getUsername(), usuario.getPassword(), AuthorityUtils.createAuthorityList(usuario.getRole().name()));
+        this.usuario = usuario;
+    }
+
+    public Long getId() {
+        return this.usuario.getId();
+    }
+
+    public String getRole() {
+        return this.usuario.getRole().name();
+    }
+
+}
